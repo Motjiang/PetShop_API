d395a52526c41dd127484a4d0f3cb9e72301fe16
Motjiang <motjiangbrilliant@gmail.com>
2024-10-24 05:46:06 +0200

    Added Product Entity

---
+++ PetShop_API/Models/Domain/ProductEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetShop_API.Models.Domain
+{
+    public class ProductEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+    }
+}
