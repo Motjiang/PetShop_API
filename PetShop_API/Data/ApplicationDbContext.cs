ef20c4a20e694b99ecbca54dd31715bcf546a902
Motjiang <motjiangbrilliant@gmail.com>
2024-10-24 05:50:27 +0200

    Added application DbContext and Depenencies

---
+++ PetShop_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop_API.Models.Domain;
+
+namespace PetShop_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+    }    
+}
