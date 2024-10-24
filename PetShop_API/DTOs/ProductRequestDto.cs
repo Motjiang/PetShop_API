f7eb6bfe5302fc01f78dfb23692d48421ccab41f
Motjiang <motjiangbrilliant@gmail.com>
2024-10-24 06:13:08 +0200

    Added a ProductRequestDto

---
+++ PetShop_API/DTOs/ProductRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PetShop_API.DTOs
+{
+    public class ProductRequestDto
+    {
+        //Properties - Brand, Title
+        public string Brand { get; set; }
+        public string Title { get; set; }
+
+    }
+}
